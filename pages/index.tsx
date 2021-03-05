5aa14cd9574dbd28fc9c807a5b6d924649104822
Oops61rus <antodeni@rambler.ru>
2021-03-06 02:23:30 +0300

    1:
    add material-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Button} from "@material-ui/core";
 
 const Index = () => {
     return (
-        <div>
-            Main page
-        </div>
+        <>
+            <div className='center'>
+                <h1>Welcome</h1>
+                <h3>This is the best music collection</h3>
+            </div>
+
+            <style jsx>
+                {`
+                    .center {
+                        margin-top: 150px;
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        justify-content: center;
+                    }                
+                `}
+            </style>
+        </>
     );
 };
 
