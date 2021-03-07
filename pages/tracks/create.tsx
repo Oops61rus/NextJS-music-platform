65e3c0749b7aba5fbc3999838ccd6e60d3a20a54
Oops61rus <antodeni@rambler.ru>
2021-03-07 19:52:14 +0300

    5:
    add steps to create file page

--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainLayout from "../../layouts/MainLayout";
+import StepWrapper from "../../components/Steps/StepWrapper";
+import {Button, Grid} from "@material-ui/core";
+import TrackInfo from "../../components/Steps/TrackInfo";
+import FileUpload from "../../components/Steps/FileUpload";
 
 const Create = () => {
+    const [activeStep, setActiveStep] = useState(0)
+    const [picture, setPicture] = useState(null)
+    const [audio, setAudio] = useState(null)
+
+    const next = () => {
+        setActiveStep(prev => prev + 1)
+    }
+
+    const back = () => {
+        setActiveStep(prev => prev - 1)
+    }
+
     return (
         <MainLayout>
-            <h1>Load track</h1>
+            <StepWrapper activeStep={activeStep}>
+                {activeStep === 0 &&
+                <TrackInfo/>
+                }
+                {activeStep === 1 &&
+                <FileUpload setFile={setPicture} accept='image/*'>
+                    <Button>Upload Cover</Button>
+                </FileUpload>
+                }
+                {activeStep === 2 &&
+                <FileUpload setFile={setAudio} accept='audio/*'>
+                    <Button>Upload Track</Button>
+                </FileUpload>
+                }
+            </StepWrapper>
+            <Grid container justifyContent='space-between'>
+                <Button disabled={activeStep === 0} onClick={back}>Back</Button>
+                <Button disabled={activeStep === 2} onClick={next}>Next</Button>
+            </Grid>
         </MainLayout>
     );
 };
