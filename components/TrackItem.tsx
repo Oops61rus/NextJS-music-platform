16a89da599e231ec56a57e0ea88c630aa8b25ec6
Oops61rus <antodeni@rambler.ru>
2021-03-07 19:17:42 +0300

    4: create more details page

--- components/TrackItem.tsx
+++ components/TrackItem.tsx
@@ -3,6 +3,7 @@ import {ITrack} from "../types/track";
 import styles from '../styles/TrackItem.module.scss'
 import {Card, Grid, Icon, IconButton} from "@material-ui/core";
 import {Delete, Pause, PlayArrow} from "@material-ui/icons";
+import {useRouter} from "next/router";
 
 interface TrackItemProps {
     track: ITrack;
@@ -10,9 +11,10 @@ interface TrackItemProps {
 }
 
 const TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
+    const router = useRouter()
     return (
-        <Card className={styles.track}>
-            <IconButton>
+        <Card className={styles.track} onClick={() => router.push('/tracks/' + track._id)}>
+            <IconButton onClick={e => e.stopPropagation()}>
                 {active
                     ? <Pause/>
                     : <PlayArrow/>
@@ -24,11 +26,11 @@ const TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
                 <div style={{fontSize: 12, color: 'gray'}}>{track.artist}</div>
             </Grid>
             {active && <div>02:42 / 03:22</div>}
-            <IconButton style={{marginLeft: 'auto'}}>
+            <IconButton onClick={e => e.stopPropagation()} style={{marginLeft: 'auto'}}>
                 <Delete/>
             </IconButton>
         </Card>
     );
-};
+}
 
 export default TrackItem;
