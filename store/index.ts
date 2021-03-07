393c4b45f0116bb60ace0710e47b28929556dc57
Oops61rus <antodeni@rambler.ru>
2021-03-07 23:20:23 +0300

    7:
    create store with wrapper

---
+++ store/index.ts
@@ -0,0 +1,9 @@
+// create a makeStore function
+import {Context, createWrapper, MakeStore} from "next-redux-wrapper";
+import {createStore} from "redux";
+import {reducer, RootState} from "./reducers";
+
+const makeStore: MakeStore<RootState> = (context: Context) => createStore(reducer);
+
+// export an assembled wrapper
+export const wrapper = createWrapper<RootState>(makeStore, {debug: true});
