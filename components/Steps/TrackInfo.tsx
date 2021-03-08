fae153864f68cc15664d7092d2747f0e4ed30bea
Oops61rus <antodeni@rambler.ru>
2021-03-08 11:48:00 +0300

    12: use created hook, and add props to TrackInfo.tsx

--- components/Steps/TrackInfo.tsx
+++ components/Steps/TrackInfo.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {Grid, TextField} from "@material-ui/core";
+import {TrackParams} from "../../types/track";
 
-const TrackInfo = () => {
+interface TrackInfoProps {
+    name: TrackParams,
+    artist: TrackParams,
+    text: TrackParams
+}
+
+const TrackInfo: React.FC<TrackInfoProps> = ({name, artist, text}) => {
     return (
         <Grid container direction={'column'} style={{padding: 20}}>
             <TextField
+                {...name}
                 style={{marginTop: 10}}
                 label={'Track name'}
             />
             <TextField
+                {...artist}
                 style={{marginTop: 10}}
                 label={'Author name'}
             />
             <TextField
+                {...text}
                 style={{marginTop: 10}}
                 label={'Track text'}
                 multiline
--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,7 +1,10 @@
 import React, {useState} from 'react';
+import {useRouter} from "next/router";
+import {useInput} from "../../hooks/useInput";
+import axios from "axios";
+import {Button, Grid} from "@material-ui/core";
 import MainLayout from "../../layouts/MainLayout";
 import StepWrapper from "../../components/Steps/StepWrapper";
-import {Button, Grid} from "@material-ui/core";
 import TrackInfo from "../../components/Steps/TrackInfo";
 import FileUpload from "../../components/Steps/FileUpload";
 
@@ -9,9 +12,25 @@ const Create = () => {
     const [activeStep, setActiveStep] = useState(0)
     const [picture, setPicture] = useState(null)
     const [audio, setAudio] = useState(null)
+    const name = useInput('')
+    const artist = useInput('')
+    const text = useInput('')
+    const router = useRouter()
 
     const next = () => {
-        setActiveStep(prev => prev + 1)
+        if (activeStep !== 2) {
+            setActiveStep(prev => prev + 1)
+        } else {
+            const formData = new FormData()
+            formData.append('name', name.value)
+            formData.append('artist', artist.value)
+            formData.append('text', text.value)
+            formData.append('picture', picture)
+            formData.append('audio', audio)
+            axios.post('http://localhost:4000/tracks', formData)
+                .then(response => router.push('/tracks'))
+                .catch(e => console.log(e))
+        }
     }
 
     const back = () => {
@@ -22,7 +41,7 @@ const Create = () => {
         <MainLayout>
             <StepWrapper activeStep={activeStep}>
                 {activeStep === 0 &&
-                <TrackInfo/>
+                <TrackInfo name={name} artist={artist} text={text}/>
                 }
                 {activeStep === 1 &&
                 <FileUpload setFile={setPicture} accept='image/*'>
@@ -37,7 +56,7 @@ const Create = () => {
             </StepWrapper>
             <Grid container justifyContent='space-between'>
                 <Button disabled={activeStep === 0} onClick={back}>Back</Button>
-                <Button disabled={activeStep === 2} onClick={next}>Next</Button>
+                <Button onClick={next}>Next</Button>
             </Grid>
         </MainLayout>
     );
